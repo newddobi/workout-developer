b05c2f5454ded9cc43a7e4174a425bf6884cb485
ddobi <qhdqhdekd261@naver.com>
2023-11-30 21:35:05 +0900

    로그인 화면을 수정한다
    이메일과 패스워드 등 영어를 한글로 수정했습니다.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "primary-color": "#2173bb",
+        "secondary-color": "#84c5ff",
+        "background-color": "#2f3437",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
